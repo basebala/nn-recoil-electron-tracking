f05fe43621650682d7c5ac93c67c7ba0676104b4
basebala <basebala@users.noreply.github.com>
2023-10-16 11:50:08 -0700

    Update ExampleSavioSubmitScript_Simulation.sh

--- savio/ExampleSavioSubmitScript_Simulation.sh
+++ savio/ExampleSavioSubmitScript_Simulation.sh
@@ -3,25 +3,25 @@
 # Remember:
 # Submit via: sbatch ...
 
-#SBATCH -J Sim
+SBATCH -J Sim
 
-#SBATCH --account=fc_cosi
-#SBATCH --partition=savio3
-#SBATCH --qos=savio_normal
+SBATCH --account=chandna
+SBATCH --partition=savio3
+SBATCH --qos=savio_normal
 
 #SBATCH --chdir=/global/scratch/users/zoglauer/Sims/EnergyLossEstimate
 
 # This should give us 1,000,000 events 
-#SBATCH -t 10:00:00
+SBATCH -t 10:00:00
 
-#SBATCH -N 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=32
+SBATCH -N 1
+SBATCH --ntasks-per-node=1
+SBATCH --cpus-per-task=32
 
-#SBATCH --signal=2@60
+SBATCH --signal=2@60
 
-##SBATCH --mail-user=XYZ@berkeley.edu
-##SBATCH --mail-type=ALL
+SBATCH --mail-user=chandna@berkeley.edu
+SBATCH --mail-type=ALL
 
 
 echo "Starting submit on host ${HOST}..."
